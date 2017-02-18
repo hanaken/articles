c5a6e773c62696c130b56df2d1004d4fff2f569b
hanaken <hanadakento@liaro.me>
2017-02-18 18:31:20 +0900

    Add spider and formatter

---
+++ articles/spiders/npb.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.selector import Selector
+
+
+class NpbSpider(CrawlSpider):
+    name = 'npb'
+    allowed_domains = ['npb.jp']
+    start_urls = ['http://npb.jp/scores/2016/0619/d-f-03/index.html']
+
+    """
+    rules = (
+        Rule(LinkExtractor(allow=r'/scores/.+/playbyplay\.html'), callback='parse_item', follow=True),
+    )
+    """
+    rules = (
+        Rule(LinkExtractor(allow='/scores/2016/0619/d-f-03/playbyplay.html'), callback='parse_item', follow=True),
+    )
+
+    def parse_box(self, response):
+        pass
+
+    def get_element_text(self, elements, xpath, index=0):
+        body = elements[index].extract()
+        return Selector(text=body).xpath(xpath).extract_first()
+
+    def get_board_scores(self, response):
+        scores = {}
+        score_tables = response.xpath('//*[@id="game_stats"]/div[2]/div/table/thead/tr/*')
+        score_tables_1 = response.xpath('//*[@id="game_stats"]/div[2]/div/table/tbody/tr[1]/*')
+        score_tables_2 = response.xpath('//*[@id="game_stats"]/div[2]/div/table/tbody/tr[2]/*')
+        team1 = self.get_element_text(score_tables_1, '//th/span/text()')
+        team2 = self.get_element_text(score_tables_2, '//th/span/text()')
+        for index, score in enumerate(score_tables):
+            if index == 0:
+                continue
+            body = score.extract()
+            title = Selector(text=body).xpath('//th/text()').extract_first()
+            if title:
+                point = self.get_element_text(score_tables_1, '//td/text()', index)
+                scores["{}回表".format(title)] = {"point": point, "team": team1}
+                point = self.get_element_text(score_tables_2, '//td/text()', index)
+                scores["{}回裏".format(title)] = {"point": point, "team": team2}
+            else:
+                title = Selector(text=body).xpath('//td/text()').extract_first()
+                if title == "計":
+                    pass
+                elif title == "H":
+                    pass
+                elif title == "E":
+                    pass
+        return scores
+
+    def parse_item(self, response):
+        item = {}
+        # TODO: 試合結果クローリング
+        #item = scrapy.Request('http://npb.jp/scores/2016/0619/d-f-03/box.html', callback=parse_box)
+        scores = self.get_board_scores(response)
+        board_scores = []
+        score_tables = response.xpath('//div[@id="progress"]/*')
+        count = ""
+        battings = []
+        for score in score_tables:
+            body = score.extract()
+            line = Selector(text=body).xpath('//td/text()').extract()
+            if not line:
+                if count and battings:
+                    board_scores.append({
+                        "count": count, "point": scores[count]["point"],
+                        "team": scores[count]["team"], "batting": battings
+                    })
+                    battings = []
+                body = score.extract()
+                count = Selector(text=body).xpath('//h5/text()').extract_first()
+                continue
+            if not count:
+                continue
+            if line[0][1:5] == "投手交代":
+                # TODO: 投手交代考慮
+                result_point = line[5].strip().split("（打点")
+                if len(result_point) > 1:
+                    result = result_point[0]
+                    point = result_point[1][0]
+                else:
+                    result = result_point[0]
+                    point = 0
+                batting = {
+                    "out": line[1].strip(),
+                    "base": line[2].strip(),
+                    "name": line[3].strip(), # 	代打・エルドレッド
+                    "ball_strike": line[4].strip(),
+                    "result": result,
+                    "point": point
+                }
+            else:
+                result_point = line[4].strip().split("（打点")
+                if len(result_point) > 1:
+                    result = result_point[0]
+                    point = result_point[1]
+                else:
+                    result = result_point[0]
+                    point = 0
+                batting = {
+                    "out": line[0].strip(),
+                    "base": line[1].strip(),
+                    "name": line[2].strip(),
+                    "ball_strike": line[3].strip(),
+                    "result": result,
+                    "point": point
+                }
+            battings.append(batting)
+        else:
+            board_scores.append({
+                "count": count, "point": scores[count]["point"],
+                "team": scores[count]["team"], "batting": battings
+            })
+
+        return board_scores
---
+++ baseball_text_formatter.py
@@ -0,0 +1,128 @@
+import re
+import sys
+import glob
+import pickle
+from collections import Counter
+
+
+# format for baseball named entity
+team = r"日本ハム|広島|オリックス|楽天|ロッテ|ソフトバンク|西武|巨人|中日|阪神|ヤクルト|DeNA|ＤｅＮＡ"
+
+place = r"マツダスタジアム|札幌ドーム|京セラドーム大阪|QVCマリン|西武プリンスドーム|甲子園|東京ドーム|ナゴヤドーム|ヤフオクドーム"
+
+day = r"[0-9]{1,2}日"
+
+state = r"(無|1|2)+死(1塁|2塁|3塁|1、2塁|1、3塁|2、3塁|満塁)" #+|得点圏"
+
+batting_order = r"[1-9]番"
+batting_position = r"(((右|中|左)翼|ライト|センター|レフト|ピッチャー|キャッチャー|ファースト|セカンド|サード|ショート)(前|越え|間)?)|((右|左|中|遊|遊撃|投|捕|1|2|3|遊){1,2}(前|越え|間))"
+batting_result = r"(([1-3]塁)?適時打)|(適時([1-3]塁)打)|犠飛|(押し出し)?4球|(セーフティ)?スクイズ|(セーフティ)?バント|犠打|死球|ゴロ|フライ" # 3振を抜いた
+batting_pos_res = r"(右|左|中|遊|遊撃|投|捕|1|2|3|遊)(犠打|犠飛|飛)"
+
+count = r"(\d+|初|延長(10|11|12))回"
+
+homerun_result = r"[0-9]+号((ランニング|満塁)(ホームラン|本塁打)|満塁弾|(([23]ラン|ソロ)(ホームラン|本塁打)?))"
+
+#point = r"同点|無(失|得)点|勝ち越し|先制|逆転|追?加点|サヨナラ"
+
+pitcher = r"先発|中継ぎ|抑え|継投"
+
+game_result = r"[0-9]+－[0-9]+"
+
+
+def format_text(text):
+    # fix number format
+    text = re.sub(r'０', "0", text)
+    text = re.sub(r'一|１', "1", text)
+    text = re.sub(r'二|２', "2", text)
+    text = re.sub(r'三|３', "3", text)
+    text = re.sub(r'四|４', "4", text)
+    text = re.sub(r'五|５', "5", text)
+    text = re.sub(r'六|６', "6", text)
+    text = re.sub(r'七|７', "7", text)
+    text = re.sub(r'八|８', "8", text)
+    text = re.sub(r'九|９', "9", text)
+
+    # fix string format
+    text = re.sub(team, "[チーム名]", text)
+    text = re.sub(place, "[試合会場]", text)
+    text = re.sub(batting_order, "[打順]", text)
+    text = re.sub(batting_position, "[打撃位置]", text)
+    text = re.sub(homerun_result, "[本塁打結果]", text)
+    text = re.sub(batting_result, "[打撃結果]", text)
+    text = re.sub(batting_pos_res, "[打撃位置][打撃結果]", text)
+    #text = re.sub(point, "[得点]", text)
+    #text = re.sub(pitcher, "[投手]", text)
+    text = re.sub(day, "[日付]", text)
+    text = re.sub(state, "[状況]", text)
+    text = re.sub(count, "[回数]", text)
+    text = re.sub(game_result, "[試合結果]", text)
+    text = re.sub(r"[0-9]+", "[数字]", text)
+    text = re.sub(r"奪[数字]振", "奪三振", text) # 奪三振用
+    return text
+
+def read_files():
+    files = glob.glob("./data/*")
+    items = []
+    for file_path in files:
+        with open(file_path, "rb") as f:
+            item = pickle.load(f)
+        items.append(item)
+    return items
+
+def format_players(item, text):
+    players = []
+    for team_name, team_players in item.items():
+        if team_name in text:
+            players.extend(team_players)
+        if team_name == "DeNA" and "ＤｅＮＡ" in text:
+            players.extend(team_players)
+
+    long_name_list = [] # 大谷翔平
+    middle_name_list = [] # 大谷翔
+    short_name_list = [] # 大谷
+    for player in players:
+        if len(player) > 1:
+            long_name_list.append(player[0]+player[1])
+            middle_name_list.append(player[0]+player[1][0])
+        short_name_list.append(player[0])
+
+    for name in long_name_list+middle_name_list+short_name_list:
+        text = re.sub(name, "[選手名]", text)
+    return text
+
+def read_players_name():
+    with open("./players_dataset.pickle", "rb") as f:
+        item = pickle.load(f)
+    return item
+
+items = read_files()
+players_item = read_players_name()
+cnt = 0
+sentence_count = Counter()
+for item in items:
+    text = item["body"]
+    norm_text = format_players(players_item, text)
+    norm_text = format_text(norm_text)
+    sentences = norm_text.split("。")
+    for sentence in sentences:
+        sentence_count[sentence] += 1
+
+    """
+    print(text)
+    print("*****************************************************")
+    print(norm_text)
+    print("*****************************************************")
+    cnt += 1
+    if cnt > 10:
+        break
+    """
+
+data_str = "文,カウント\n"
+for sent, count in sorted(sentence_count.items(), key=lambda x:x[1], reverse=True):
+    if not sent.strip():
+        continue
+    data_str += "{0},{1}\n".format(sent, count)
+
+with open("./rank.csv", "w") as f:
+    f.write(data_str)
